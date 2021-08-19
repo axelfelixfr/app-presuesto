a17f6a801ee2649e61aa99b3b74b5abd8b1df47d
Axel <axelfelix123@hotmail.com>
2021-08-18 19:09:37 -0500

    Componente pregunta y maquetado principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Pregunta from './components/Pregunta';
 
 function App() {
-  return <h1>Presupuesto</h1>;
+  return (
+    <div className='container'>
+      <header>
+        <h1>Gasto semanal</h1>
+
+        <div className='contenido-principal contenido'>
+          <Pregunta />
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
